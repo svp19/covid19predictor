dc060b92f002c58227fbeb959cade3b54a4e8569
neuralflux <anudeep.tumati99@gmail.com>
2020-05-25 14:12:01 +0530

    debug false

--- covid19predictor/settings.py
+++ covid19predictor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%ku$xnlrl+jiveqhl#ib6s&^cuyq-d$beryu**dc(na%r13wyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cov19-predictor-delta.herokuapp.com']
 
